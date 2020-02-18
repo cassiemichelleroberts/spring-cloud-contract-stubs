955a0de450ed9d55cd8758113a947c603ff0f089
Cassie Roberts <cassieroberts@Cassies-MacBook-Pro.local>
2020-02-18 15:12:03 -0500

    make age dynamic

--- META-INF/com.example.contract/producer/0.0.1-SNAPSHOT/contracts/shouldAllowBeerIfOldEnough.groovy
+++ META-INF/com.example.contract/producer/0.0.1-SNAPSHOT/contracts/shouldAllowBeerIfOldEnough.groovy
@@ -1,21 +1,29 @@
 import org.springframework.cloud.contract.spec.Contract
 
 Contract.make {
+    description('''
+        given:
+        	An inout is received
+        when:
+        	something
+        then:
+        	Output
+    ''')
     request {
         method 'PUT'
         url '/check'
-        body([
-                "age": 21
-        ])
+        body(
+                age: value(consumer(regex('[2-9][0-9]')))
+        )
         headers {
             contentType('application/json')
         }
     }
     response {
         status OK()
-        body([
+        body(
                 beerCheckStatus: "OKAY"
-        ])
+        )
         headers {
             contentType('application/json')
         }
